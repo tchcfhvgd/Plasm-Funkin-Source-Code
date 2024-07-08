237f27082fdf83236b28f6ed5711627435c8b0ed
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-08 21:33:15 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -17,7 +17,9 @@ import lime.app.Application;
 import openfl.events.UncaughtErrorEvent;
 import haxe.CallStack;
 import haxe.io.Path;
+#if desktop
 import Discord.DiscordClient;
+#end
 import sys.FileSystem;
 import sys.io.File;
 import sys.io.Process;
